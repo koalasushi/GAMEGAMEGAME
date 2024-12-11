973c5eccb1a3e51b2ccfea6435241fd68f09726b
koalasushi <kai.daniel.travis@gmail.com>
2024-12-11 16:00:08 +0900

    キャラ、カーテン、簡単な操作の追加

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Character : MonoBehaviour
+{
+    private CharacterController characterController;
+    public float gravity = 90;
+    public float jumpSpeed = 15;
+    public float Speed = 5f;
+    Vector3 moveVelocity;
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+
+        if(characterController.isGrounded){
+            moveVelocity = new Vector3(Speed * hInput, 0, Speed * vInput);
+            if(Input.GetButton("Jump")){
+                moveVelocity.y = jumpSpeed;
+            }
+        }
+
+        moveVelocity.y -= gravity * Time.deltaTime;
+        characterController.Move(moveVelocity * Time.deltaTime);
+    }
+}
